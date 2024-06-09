3a11114e4310d202b76296a00803e59a8bb8c446
rusgyn <rusgynmagsanay@gmail.com>
2024-06-09 19:13:58 -0400

    Implent the code for survey app.

--- survey.js
+++ survey.js
@@ -1,5 +1,7 @@
 // a quick survey app which asks the user a bunch of questions like their favourite music, activity, food, sport, etc. It will then generate a profile description for them to use online.
 
+// Example of a "Callback hell" or "callback Waterfall"
+
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -7,8 +9,45 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("What do you think of Node.js? ", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const answers = [];
 
-  rl.close();
+process.stdout.write("\nDISCLAIMER\n\nYour answers will be held in strict confidentiality and will be used only for the purposes of this study. The results will be reported in aggregate form only, and cannot be identified individually.\n\nThere are 7 survey questions only. Let's start.\n\n");
+
+rl.question('1.What\'s your name? ', (answer) => {
+  console.log(`Thank you for your valuable answer:   ${answer}\n`);
+  answers.push(`Survey respondent name is ${answer}.`);
+
+  rl.question('2. What\'s an activity you like doing? ', (answer) => {
+    console.log(`Thank you for your valuable answer:   ${answer}\n`);
+    answers.push(`Who like doing ${answer}`);
+
+    rl.question('3. What do you listen to while doing that? ', (answer) => {
+      console.log(`Thank you for your valuable answer:   ${answer}\n`);
+      answers.push(`and listen to ${answer} while doing his favorite activity.`);
+
+      rl.question('4. Which meal is your favourite ? ', (answer) => {
+        console.log(`Thank you for your valuable answer:   ${answer}\n`);
+        answers.push(`Our respondent favourite meal is ${answer}`);
+
+        rl.question('5. What\'s your favourite thing to eat for that meal? ', (answer) => {
+          console.log(`Thank you for your valuable answer:   ${answer}\n`);
+          answers.push(`and loves to eat ${answer}.`);
+
+          rl.question('6. Which sport is your absolute favourite? ', (answer) => {
+            console.log(`Thank you for your valuable answer:   ${answer}\n`);
+            answers.push(`Aside from that, our respondent is sporty and loves ${answer}.`);
+
+            rl.question('7. What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+              console.log(`Thank you for your valuable answer:   ${answer}\n`);
+              answers.push(`Above all, has the ${answer} superpower.`);
+
+              console.log(`\n\n${answers.join(' ')}\n\n<b>Thank you for participating the survey.</b>\n`);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
 });
